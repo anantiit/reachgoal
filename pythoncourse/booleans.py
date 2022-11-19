9546b027a070cf59418b140d55194971d0b197b6
anantiit <anant123.iit@gmail.com>
2022-11-19 16:13:48 +0530

    making separate repo for python

--- pythoncourse/booleans.py
+++
@@ -1,8 +0,0 @@
-print(not True)
-print(not False)
-name = input("Enter your name: ")
-age = int(input("Enter your age :"))
-if(age<18 or age>60):
-    print(f"{name} is not working")
-else:
-    print(f"{name} is working")
--- pythoncourse/lists.py
+++
@@ -1,5 +0,0 @@
-friends=["Naidu", "prakash", "suresh"]
-print(friends)
-friends.append("sumit")
-friends.remove("prakash")
-print(friends)
